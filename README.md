983623971a62e11be039a16bb16cbeda6178e922
Matthew Schulz <74387737+mkschulz9@users.noreply.github.com>
2023-09-05 18:47:12 -0700

    feat: add ranking function & update readme - implemented ranking function, updated readme, & edited genInitPop func

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# GeneticAlgorithm Repository
+ # GeneticAlgorithm Repository
 
 ## Main Idea
-- This repository holds assignment #1 for CSCI-561, Foundations of Artificial Intelligence, which focuses on solving a 3D Travelling Salesman Problem (TSP) using a genetic algorithm. Here, the AI agent finds the shortest path through a set of cities with 3D coordinates. The agent starts at a specified "home" location, travels to various cities, and returns home, aiming to minimize the distance traveled. Key aspects include implementing initial population generation, parent selection, crossover between parents, and mutation.
+This repository holds assignment #1 for CSCI-561, Foundations of Artificial Intelligence, which focuses on solving a 3D Travelling Salesman Problem (TSP) using the genetic algorithm. Here, the AI agent is given a list of cities represented by coordinates of the format (x, y, z). It then employs the genetic algorithm to find the shortest path through a set of cities, starting and ending at the same city while visiting all other cities exactly once. Key aspects of this agent include:
+- Initial generation of paths
+- Path ranking
+- Crossover between best paths
+- And mutation of path 'offspring'
+
+This project is implemented entirely from scratch in Python.
+
+## Travelling Sales Person Problem
+
+## Genetic Algorithm
+
+## Implementation of Genetic Algorithm (main.py)
+### Key Functions:
+#### calculateDistance()
+- Accepts two coordinates of any dimension and returns the Euclidean distance between the two points. In this project’s scope, it is used to calculate the distance between two cities in the 3D space.
+
+#### generateInitialPopulation()
+- Generates unique, valid paths that start and end at the same city and visit all other cities exactly once. It is used to seed the genetic algorithm with a population of paths that can be used to create future generations.
+
+#### rankPopulation()
+- Ranks the given population. It finds the distance required to travel each path in the population and returns a sorted list of paths in ascending order of distance.
+
--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import random
 import math
 from scipy.spatial.distance import euclidean
 
+# change defualt behavior of print function to print a new line after each call
+def printWithNewline(*args, **kwargs):
+    kwargs['end'] = kwargs.get('end', '\n') + '\n'
+    print(*args, **kwargs)
+
 # opens the input file and reads its content
 # input: none
 # output: a list containing the number of cities and a list of tuples containing the coordinates of each city
 def openInputFile():
     cityCoordinates = []
 
-    # Open the file and read its content
+    # open the file and read its content
     with open("./io/inputs/input1.txt", "r") as file:
         numberOfCities = int(file.readline().strip())
 
@@ -16,7 +21,7 @@ def openInputFile():
             x, y, z = map(int, line.strip().split())
             cityCoordinates.append((x, y, z))
             
-    print("NUMBER OF CITIES:", numberOfCities)
+    printWithNewline("NUMBER OF CITIES TO VISIT:", numberOfCities)
 
     return [numberOfCities, cityCoordinates]
 
@@ -27,18 +32,38 @@ def calculateDistance(coordinate1, coordinate2): return(euclidean(coordinate1, c
 
 # generates a list of initial paths
 # input: the number of paths to generate, the number of cities, list of city coordinates
-# output: list of paths = size
-def generateInitialPopulation(size, numberOfCities, cityList):
+# output: list of unique paths = size
+def generateInitialPopulation(size, numberOfCities, cityList): # check back here to try optimizing & think about heuristic to use (Check back to see if uniqueness matters & to generate a better population, maybe once half of the size of pop is generated, find the average distance for each already egenerated path, and only accept new paths below that average, etc.)
     population = []
-    for i in range(size):
-        population.append(random.sample(cityList, numberOfCities))
-        population[i].append(population[i][0])
-    print("INITIAL POPULATION GENERATED.", "SIZE:", size)
-    return(population)
+    seenPaths = set()
+    
+    while len(population) < size:
+        path = random.sample(cityList, numberOfCities)
+        pathTuple = tuple(path)
+        
+        if pathTuple not in seenPaths:
+            seenPaths.add(pathTuple)
+            path.append(path[0])
+            population.append(path)
+            
+    printWithNewline(f"INITIAL POPULATION GENERATED (SIZE: {size})")
+    return population
 
-def rank_population(population):
-    # Evaluate the fitness of each individual in the population
-    pass
+# calculates the total distance of each path in the population and orders them from shortest to longest
+# input: size of population, population list, number of cities to visit
+# output: ranked list of population
+def rankPopulation(sizeOfPopulation, population, numberOfCities):
+    rankedPopulation = []
+    
+    for i in range(sizeOfPopulation): # check back here to try optimizing
+        totalDistance = 0
+        for j in range(numberOfCities):
+            totalDistance += calculateDistance(population[i][j], population[i][j+1])
+        rankedPopulation.append([population[i], totalDistance])
+    
+    rankedPopulation.sort(key=lambda x: x[1])
+    printWithNewline(f"POPULATION RANKED (BEST PATH DISTANCE: {rankedPopulation[0][1]})")
+    return rankedPopulation # change rank function to only return the nth best paths (no need to pass back all paths)
 
 def create_mating_pool(population, rank_list):
     # Select parents for crossover
@@ -94,10 +119,13 @@ def genetic_algorithm():
         # ...
 
 def test():
-    # print(calculateDistance((0, 0, -1), (10, 100, 30)))
+    # printWithNewline(calculateDistance((0, 0, -1), (10, 100, 30)))
     numberOfCitiesAndCityList = openInputFile()
-    initialPopulationSize = 20
-    print(generateInitialPopulation(initialPopulationSize, numberOfCitiesAndCityList[0], numberOfCitiesAndCityList[1]))
+    initialPopulationSize = 5000
+    rankPopulation(initialPopulationSize, generateInitialPopulation(initialPopulationSize, numberOfCitiesAndCityList[0], 
+                                                                                     numberOfCitiesAndCityList[1]), numberOfCitiesAndCityList[0])
+    
+    # printWithNewline(rankPopulation(initialPopulationSize, generateInitialPopulation(initialPopulationSize, 5, [(0, 0, 0), (10, 0, 30), (20, 0, 60), (30, 0, 90), (40, 0, 120)]), 5))
     
 if __name__ == "__main__":
     test()
